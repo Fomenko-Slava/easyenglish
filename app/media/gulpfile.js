44aab77dc3ed57c15bc3c0b8237b5be1086dbd38
Slava <vyacheslav.fomenko@attractgroup.com>
2018-10-08 13:33:17 +0300

    nginx container

---
+++ app/media/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var cssmin      = require('gulp-clean-css');
+var concat      = require('gulp-concat');
+var sourcemaps  = require('gulp-sourcemaps');
+var gulpIf      = require('gulp-if');
+
+const isProd = process.env.ENV && process.env.ENV == 'prod';
+const isLocal = process.env.ENV && process.env.ENV == 'loc';
+
+console.log(process.env.ENV);
+
+gulp.task('style', function () {
+    return gulp.src('src/css/main.scss')
+        .pipe(gulpIf(!isProd, sourcemaps.init()))
+        .pipe(sass())
+        .pipe(cssmin())
+        .pipe(gulpIf(!isProd, sourcemaps.write()))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/css/**/*.scss', gulp.series('style'));
+});
+
+if (isLocal) {
+    gulp.task('default', gulp.series('style', 'watch'));
+} else {
+    gulp.task('default', gulp.series('style'));
+}
+
---
+++ run.sh
@@ -0,0 +1,14 @@
+export ENV=loc                                    # окружение проекта
+export PROJECT_NAME=my_shop                            # название проекта
+export COMPOSE_PROJECT_NAME=sw-${PROJECT_NAME}      # название проекта в docker
+export APP_URL=${PROJECT_NAME}.${ENV}               # урл проекта будет в VIRTUAL_HOST
+export COMPOSE_FILE="docker-compose.build.yml"      # указываем какой файл использовать
+export COMPOSE_FILE_DEPLOY="docker-compose.${ENV}.swarm.yml" # файл для deploy
+export BUILD_NUMBER=latest
+
+#docker-compose -f ${COMPOSE_FILE} config
+docker-compose build --force-rm --no-cache #будет браться указанный файл из переменной окружения
+
+#docker-compose -f ${COMPOSE_FILE_DEPLOY} config
+#docker stack deploy -c ${COMPOSE_FILE_DEPLOY} ${COMPOSE_PROJECT_NAME} # запуск проекта
+#docker-compose -f docker-compose.loc.swarm.yml up
