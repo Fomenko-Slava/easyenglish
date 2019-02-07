5c8a4e293161b6947fbc00aee6b0f2e1918c408a
Slava <vyacheslav.fomenko@attractgroup.com>
2019-02-07 23:19:39 +0200

    add js

--- app/media/src/js/plugins.js
+++ app/media/src/js/plugins.js
@@ -2,13 +2,22 @@ app.plugins = (function ($) {
 
     function init() {
         navbar_menu();
+
+
     }
     
     function navbar_menu() {
         $('[data-toggle="slide-collapse"]').on('click', function() {
-            alert('123');
             $navMenuCont = $($(this).data('target'));
-            $navMenuCont.animate({'width':'toggle'}, 350);
+            $navMenuCont.css('z-index', 1000).animate({'left':'0'}, 200);
+            $('#overlay_bg').addClass('active');
+            $('.nav_menu_line').removeClass('sticky-top');
+        });
+
+        $('#overlay_bg, #close_left_sidebar').on('click', function () {
+            $('#overlay_bg').removeClass('active');
+            $('.nav_menu_line').addClass('sticky-top');
+            $('#collapsed_menu').css('z-index', 1).animate({'left':'-280px'}, 200);
         });
     }
 
